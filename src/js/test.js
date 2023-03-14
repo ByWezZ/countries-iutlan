8e410fe604c55fcaaa3a4b343c21504c9b2eebf3
ByWezZ <valentin_clm@outlook.fr>
2023-03-14 17:07:43 +0100

    Add prettierrc

--- src/js/test.js
+++ src/js/test.js
@@ -1,54 +1,60 @@
 //Q1
 function outsideTheContinent() {
-  return Object.values(Country.all_countries).filter(elem => elem.borders.some(b => elem.region !== Country.all_countries[b].region));
+    return Object.values(Country.all_countries).filter((elem) =>
+        elem.borders.some(
+            (b) => elem.region !== Country.all_countries[b].region
+        )
+    );
 }
 
 //Q2
 function moreNeighbors() {
-  res = [];
+    res = [];
 
-  let maxNeighbors = 0;
+    let maxNeighbors = 0;
 
-  Object.values(Country.all_countries).forEach(v => {
-    if (v.borders.length > maxNeighbors) {
-      maxNeighbors = v.borders.length;
-      res = [v];
-    } else if (v.borders.length == maxNeighbors) {
-      res.push(v);
-    }
-  });
+    Object.values(Country.all_countries).forEach((v) => {
+        if (v.borders.length > maxNeighbors) {
+            maxNeighbors = v.borders.length;
+            res = [v];
+        } else if (v.borders.length == maxNeighbors) {
+            res.push(v);
+        }
+    });
 
-  return res;
+    return res;
 }
 
 //Q3
 function neighborless() {
-  return Object.values(Country.all_countries).filter(elem => !elem.borders.length);
+    return Object.values(Country.all_countries).filter(
+        (elem) => !elem.borders.length
+    );
 }
 
 //Q4
 function moreLanguages() {
-  res = [];
+    res = [];
 
-  let maxLanguages = 0;
+    let maxLanguages = 0;
 
-  Object.values(Country.all_countries).forEach(v => {
-    if (v.languages.length > maxLanguages) {
-      maxLanguages = v.languages.length;
-      res = [v];
-    } else if (v.languages.length == maxLanguages) {
-      res.push(v);
-    }
-  });
+    Object.values(Country.all_countries).forEach((v) => {
+        if (v.languages.length > maxLanguages) {
+            maxLanguages = v.languages.length;
+            res = [v];
+        } else if (v.languages.length == maxLanguages) {
+            res.push(v);
+        }
+    });
 
-  return res;
+    return res;
 }
 
 //Q5
 function withCommonLanguage() {
-  res = {};
+    res = {};
 
-  return res;
+    return res;
 }
 //Q6
 function withoutCommonCurrency() {}
@@ -57,7 +63,9 @@ function sortingDecreasingDensity() {}
 
 //Q8
 function moreTopLevelDomains() {
-  return Object.values(Country.all_countries).filter(elem => elem.topLevelDomain.length > 1);
+    return Object.values(Country.all_countries).filter(
+        (elem) => elem.topLevelDomain.length > 1
+    );
 }
 
 //Q9
