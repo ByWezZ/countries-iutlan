00f1aa9db126ed5d77d34ab70f3e3dee5f536e46
ByWezZ <valentin_clm@outlook.fr>
2023-04-02 18:37:42 +0200

    Created an onFilterChange function and filter by region

--- src/js/filters.js
+++ src/js/filters.js
@@ -1,17 +1,28 @@
-function searchCountries() {
+const comboRegion = document.getElementById('region');
+const comboLanguage = document.getElementById('language');
+
+function onFilterChange() {
+    const selectedRegion = comboRegion.value;
+
+    currentDisplayedCountries = Object.values(Country.all_countries);
+
+    if (selectedRegion != '')
+        currentDisplayedCountries = currentDisplayedCountries.filter(
+            (c) => c.region == selectedRegion
+        );
+
     page = 0;
     let searchField = document.getElementById('searchbar').value;
-    currentDisplayedCountries = Object.values(Country.all_countries).filter(
-        (c) => c.name.toLowerCase().includes(searchField.toLowerCase())
+    currentDisplayedCountries = currentDisplayedCountries.filter((c) =>
+        c.name.toLowerCase().includes(searchField.toLowerCase())
     );
 
     displayCountries(getCountriesParent(), currentDisplayedCountries); //This line will be erased for mutli-criteria research
 }
 
-function populateCombobox() {
-    let comboRegion = document.getElementById('region');
-    let comboLanguage = document.getElementById('language');
+function onLanguageSelected() {}
 
+function populateCombobox() {
     // Populate Region Combo
     for (const region of Country.getAllRegions()) {
         let optionElement = document.createElement('option');
